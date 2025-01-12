95928522b4ecb0196e5a265dd7b8d53a2f7e41cc
Fabio Acosta <fabioacosta00001@gmail.com>
2025-01-12 22:01:38 +0000

    Título y favicon añadidos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="bg-gray-800 text-white p-4 shadow-lg">
             <div className="max-w-7xl mx-auto flex justify-between items-center">
-                <div className="text-2xl font-bold">ModaTrend</div>
+                <div className="text-2xl font-bold">Dafashion</div>
                 <ul className="flex space-x-6">
                     <li><Link to="/" className="hover:text-gray-400">Home</Link></li>
                     <li><Link to="/collection" className="hover:text-gray-400">Collection</Link></li>
