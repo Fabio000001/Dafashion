7d76367373ac1b1a049a19663e26dc4171c8b36e
Fabio Acosta <fabioacosta00001@gmail.com>
2025-01-13 14:47:05 +0000

    Fondos mejorados

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -4,7 +4,7 @@ const Collection = () => {
     const collections = ["Collection 1", "Collection 2", "Collection 3", "Collection 4"];
 
     return (
-        <div className="py-16 px-4 bg-gray-100">
+        <div className="py-16 px-4 bg-gray-100 background">
             <h2 className="text-4xl font-bold text-center mb-12">Nuestra Colección Exclusiva</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {collections.map((collection, index) => (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,11 @@ import './styles/Home.css';
 
 const Home = () => {
     return (
-        <div className="bg-cover bg-center h-[60vh] text-white flex items-center justify-center relative" style={{ backgroundImage: 'url("https://via.placeholder.com/1500x800?text=Fashion+Background")' }}>
+        <div className="bg-cover bg-center h-[60vh] text-white flex items-center justify-center relative bgImage">
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="relative text-center show">
-                <h1 className="text-5xl font-extrabold mt-10 mb-16">Bienvenidos al Mundo de la Moda</h1>
-                <div className='mb-16'>
+                <h1 className="text-5xl font-extrabold mb-20">Bienvenidos al Mundo de la Moda</h1>
+                <div className='mb-12'>
                     <p className="text-lg">Descubre las últimas colecciones.</p>
                     <a href="/collection" className="mt-6 inline-block bg-orange-500 px-8 py-3 rounded-full text-white font-semibold hover:bg-orange-600">Ver Colecciones</a>
                 </div>
