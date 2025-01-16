5a98cff7d85d719a502e6edb5133b25076d27984
Fabio Acosta <Fabio@Acosta.com>
2025-01-16 18:02:02 +0000

    Tendencias rellenadas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,10 @@ const Navbar = () => {
     return (
         <nav className="bg-gray-800 text-white p-4 shadow-lg">
             <div className="max-w-7xl mx-auto flex justify-between items-center">
-                <div className="text-2xl font-bold"><Link to="/" className="text-white hover:text-gray-400">Dafashion</Link></div>
+                <div className="text-2xl font-bold">
+                    <Link to="/" className="text-white hover:text-gray-400">Dafashion</Link>
+                </div>
                 <ul className="flex space-x-6">
-                    <li></li>
                     <li><Link to="/collection" className="hover:text-gray-400">Colecciones</Link></li>
                     <li><Link to="/trends" className="hover:text-gray-400">Tendencias</Link></li>
                 </ul>
--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -1,7 +1,11 @@
 import './styles/Trends.css';
 
 const Trends = () => {
-    const trends = ["Trend 1", "Trend 2", "Trend 3"];
+    const trends = [
+        { name: "Pantalón City beige", imagen: "/public/trends/pantalones-city-beige.jpg" },
+        { name: "Vestido Linyer verde", imagen: "/public/trends/vestido-linyer-verde.jpg" },
+        { name: "Abrigo de doble botonadura", imagen: "/public/trends/abrigo-doble-botonadura.jpg" },
+    ];
 
     return (
         <div className="py-16 px-4 bg-white">
@@ -9,9 +13,9 @@ const Trends = () => {
             <div className="flex flex-col sm:flex-row sm:justify-between space-y-6 sm:space-y-0 sm:space-x-6">
                 {trends.map((trend, index) => (
                     <div key={index} className="bg-gray-200 rounded-lg overflow-hidden shadow-lg w-full sm:w-1/3 slide">
-                        <img className="w-full h-56 object-cover" src={`https://via.placeholder.com/400x300?text=${trend}`} alt={trend} />
+                        <img className="w-full h-96 object-cover" src={trend.imagen} alt={trend.name} />
                         <div className="p-4">
-                            <h3 className="text-xl font-semibold">{trend}</h3>
+                            <h3 className="text-xl font-semibold">{trend.name}</h3>
                             <p className="mt-2 text-gray-600">Las tendencias más destacadas de esta temporada. ¡No te las puedes perder!</p>
                         </div>
                     </div>
